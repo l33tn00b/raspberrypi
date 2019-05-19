fcefbb840e870b756f9c37e844702b8ec71e983f
l33tn00b <daniel.barie@gmx.de>
2019-05-19 16:53:07 +0200

    Update wasser_an.py

--- wasser_an.py
+++ wasser_an.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # this is for the "master" controlling a remote pi
 # we need pigpio installed
 # we need gpiozero installed
@@ -10,4 +11,17 @@
 # Enable Remote GPIO on the Pi in the Raspberry Pi Configuration Tool.
 # Run the pigpio daemon on the Pi: sudo pigpiod
 
-
+# on the local pi:
+# need to have environment variables set
+# i.e.
+# GPIOZERO_PIN_FACTORY=pigpio
+# PIGPIO_ADDR=192.168.3.34
+from gpiozero import LED
+from signal import pause
+from gpiozero.pins.pigpio import PiGPIOFactory
+# broadcom numbbering is default
+# playing dumb. hardcoded address.
+factory = PiGPIOFactory(host='192.168.3.34')
+led = LED(18, pin_factory=factory)
+led.off()  # inverted logic
+pause()
