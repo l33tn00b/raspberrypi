b4010aa023ef211f89a0cf4eba4a0cb3dc9aae11
l33tn00b <daniel.barie@gmx.de>
2019-05-19 16:04:48 +0200

    Update wasser_an.py

--- wasser_an.py
+++ wasser_an.py
@@ -1,7 +1,13 @@
+# this is for the "master" controlling a remote pi
 # we need pigpio installed
 # we need gpiozero installed
 # so: 
 # sudo apt-get install python-pip
 # sudo pip install pigpio
 # sudo pip install gpiozero
+#
+# on the remote pi:
+# Enable Remote GPIO on the Pi in the Raspberry Pi Configuration Tool.
+# Run the pigpio daemon on the Pi: sudo pigpiod
+
 
